84dc633d205e73002709cef7a45ae9c6a1bae3cc
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-17 14:06:02 +0200

    eslint fixes

--- backend/cypress/e2e/phonebook.cy.js
+++ backend/cypress/e2e/phonebook.cy.js
@@ -1,7 +1,6 @@
 describe('Phonebook', function() {
-    it('front page can be opened', function() {
-      cy.visit('http://localhost:8080')
-      cy.contains('Phonebook')
-    })
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('Phonebook')
   })
-  
+})
