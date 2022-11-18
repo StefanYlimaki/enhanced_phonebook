7e3f8fb84805e9c893af668153c7ed3170f695c5
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-18 12:08:02 +0200

    implementing healt_check

---
+++ health_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update && apt-get install curl
+
+res=$(curl -s https://stefanpokedex.fly.dev/health)
+
+echo $res
+
+if [ "$res" == "ok" ]; then
+  echo "Succeeded curl to /health"
+  exit 0
+  fi
+
+echo "Failed curl to /health"
+# 0: OK, 1: Bad.
+exit 1
